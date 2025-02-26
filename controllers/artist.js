d7b78400adf3cbd4e88fe58d32e086747232138b
jose <josevega1999,16@gmail.com>
2025-02-25 23:14:28 -0400

    Model, Agregar y Listar Artista

--- controllers/artist.js
+++ controllers/artist.js
@@ -1,12 +1,105 @@
+// Importaciones
+const Artist = require("../models/Artists");
+const mongoosePagination = require("mongoose-pagination");
+
 // Acción de prueba
 const prueba = (req, res) => {
-    res.status(200).send({
+    return res.status(200).send({
         status: "success",
         message: "Mensaje enviado desde el controlador de Artistas."
     });
 }
 
+// Acción Guardar Artista
+const save = async (req, res) =>{
+
+    // Recoger datos del body
+    let params = req.body;
+
+    // Crear el objeto a guardar
+    const artist = new Artist(params);
+
+    // Guardar y enviar respuesta
+    try{
+        let artistStored = await artist.save();
+        console.log(artistStored);
+        return res.status(200).send({
+            status: "success",
+            message: "Artista guardado.",
+            artistStored
+        });
+
+    }catch(error){
+        return res.status(500).send({
+            status: "Error",
+            message: "Error al guardar el artista."
+        });
+    }
+};
+
+const one = async (req, res) =>{
+
+    // Sacar id de la url
+    const id= req.params.id;
+
+    // Hacer find y devolver respuesta
+    try{
+        let artist = await Artist.findById(id);
+
+        if(!artist){
+            return res.status(404).send({
+                status: "error",
+                message: "Artista no encontrado."
+            });
+        }
+
+        return res.status(200).send({
+            status: "success",
+            artist
+        });
+    }catch(error){
+        return res.status(500).send({
+            status: "error",
+            message: "Error buscando el artista."
+        });
+    }
+}
+
+const list = async (req, res) =>{
+
+    // Sacar la posible página
+    let page = 1;
+    if(req.params.page){
+        page= req.params.page;
+    }
+
+    // Definir cuantos elementos por página
+    const itemsPerPage = 2;
+
+    // Find, ordenarlo y paginarlo
+    try{
+        let artistToFind = await Artist.find().sort("name").paginate(page, itemsPerPage);
+        const total = await Artist.countDocuments();
+    
+        return res.status(200).send({
+            status: "success",
+            artistToFind,
+            page,
+            itemsPerPage,
+            total
+        });
+    }catch(error){
+        return res.status(500).send({
+            status: "success",
+            message: "Error al listar artistas."
+        });
+    }
+}
+
 // Exportar acciones
 module.exports = {
-    prueba
+    prueba,
+    save,
+    one,
+    list
 }
--- routes/artist.js
+++ routes/artist.js
@@ -1,5 +1,7 @@
 // Importar dependencias
 const express = require("express");
+const artistController = require("../controllers/artist");
+const check = require("../middlewares/auth");
 
 // Cargar Router
 const router = express.Router();
@@ -8,7 +10,10 @@ const router = express.Router();
 const artistsController = require("../controllers/artist");
 
 // Definir rutas
-router.get("/prueba-artist", artistsController.prueba)
+router.get("/prueba-artist", artistsController.prueba);
+router.post("/save", check.auth, artistController.save);
+router.get("/one/:id", check.auth, artistController.one);
+router.get("/list/:page?", check.auth, artistController.list);
 
 // Exportar rutas
 module.exports = router;
